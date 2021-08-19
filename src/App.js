6e0f6b60ff8955708e1b317ec99dca4f5a6d6bea
Jayanth <jayanthacharya4@gmail.com>
2021-08-19 17:00:30 +0530

    update time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import Digit from "./Digit";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [isDigital, setClockType] = useState(true)
-  
-  function getTime(segment) {
+  const [digitArray, setDigitArray] = useState([0, 0, 0, 0,])
+
+  function getTime() {
     let date = new Date()
-    let time = date.getHours().toString().padStart(2, '0') + date.getMinutes().toString().padStart(2, '0')
-    return time[segment]
+    let time = date.getHours().toString().padStart(2, '0')
+      + date.getMinutes().toString().padStart(2, '0')
+    setDigitArray(() => {
+      return [time[0], time[1], time[2], time[3]]
+    })
   }
 
   function toggleClock() {
     setClockType(isDigital => !isDigital);
   }
 
+  useEffect(() => {
+    const timerID = setInterval(() => getTime(), 15000)
+    return () => {
+      clearInterval(timerID)
+    }
+  }, [])
+
+  function getDigit(index) {
+    return isDigital ? 10 : digitArray[index]
+  }
+
   return (
     <div>
       <div className="App" onClick={toggleClock}>
-        <Digit value={isDigital ? 10 : getTime(0)} />
-        <Digit value={isDigital ? 10 : getTime(1)} />
-        <Digit value={isDigital ? 10 : getTime(2)} />
-        <Digit value={isDigital ? 10 : getTime(3)} />
+        <Digit value={getDigit(0)} />
+        <Digit value={getDigit(1)} />
+        <Digit value={getDigit(2)} />
+        <Digit value={getDigit(3)} />
       </div>
     </div>
   );
